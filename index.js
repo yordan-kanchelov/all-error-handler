c2fd7c6c1c6f64a73f5dd8201e28ca6e77c118f6
Jordan Kanchelov <jkanchelov@gmail.com>
2017-11-26 22:07:00 +0200

    added callback param check

--- index.js
+++ index.js
@@ -1,50 +1,54 @@
 "use strict";
 
-class AllErrorHandler {
-    constructor(callback) {
-        this.callback = callback;
-
-        this._setupEvents();
-    }
-
-    startListening() {
-        this._setupEvents();
-    }
-
-    stopListening() {
-        this._setupEvents(false);
-    }
-
-    dispose() {
-        this._setupEvents(false);
-        this.callback = null;
-    }
-
-    _setupEvents(on = true) {
-        if(on) {
-            if (typeof window !== "undefined") {
-                window.addEventListener("error", this.callback);
-            } else {
-                process.on('uncaughtException', this.callback);
-            }
-        } else {
-            if (typeof window !== "undefined") {
-                window.removeEventListener("error", this.callback, true);
-            }
-            else {
-                process.off('uncaughtException', this.callback);
-            }
-        }
-    }
-}
-
 (function () {
     var root = this
     var previous_mymodule = root.AllErrorHandler
 
-    AllErrorHandler.noConflict = function () {
-        root.AllErrorHandler = previous_mymodule
-        return AllErrorHandler
+    class AllErrorHandler {
+        constructor(callback) {
+            if(!callback) {
+                throw new Error("Missing callback function");
+            }
+
+            this.callback = callback;
+
+            this._setupEvents();
+        }
+
+        startListening() {
+            this._setupEvents();
+        }
+
+        stopListening() {
+            this._setupEvents(false);
+        }
+
+        noConflict() {
+            root.AllErrorHandler = previous_mymodule
+            return AllErrorHandler
+        }
+
+        dispose() {
+            this._setupEvents(false);
+            this.callback = null;
+        }
+
+        _setupEvents(on = true) {
+            if (on) {
+                if (typeof window !== "undefined") {
+                    window.addEventListener("error", this.callback);
+                } else {
+                    process.on('uncaughtException', this.callback);
+                }
+            } else {
+                if (typeof window !== "undefined") {
+                    window.removeEventListener("error", this.callback, true);
+                }
+                else {
+                    process.off('uncaughtException', this.callback);
+                }
+            }
+        }
     }
 
     if (typeof exports !== 'undefined') {
@@ -57,5 +61,3 @@ class AllErrorHandler {
         root.AllErrorHandler = AllErrorHandler
     }
 }).call(this);
-
-
