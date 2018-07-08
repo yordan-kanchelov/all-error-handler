dc68728915cfe8ad6b3b36f22111d9a8cf63d21d
Jordan Kanchelov <jkanchelov@gmail.com>
2018-07-08 17:42:33 +0300

    fixed problem with the umd export

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-"use strict";
-
-(function () {
-    var root = this
-    var previous_mymodule = root.AllErrorHandler
+(function() {
+    var root = this;
+    var previous_mymodule = root.AllErrorHandler;
 
     /**
      * Class providing objects to listen for uncaught errors.
@@ -10,7 +8,7 @@
     class AllErrorHandler {
         /**
          * Create a allErrorHandler object.
-         * @param {Function} callback - The callback which is called after an occured error event.
+         * @param {Function} callback - The callback which is called after an occurred error event.
          * @param {Boolean} startListening - Chose if the object should start listening immediately.
          */
         constructor(callback, startListening = true) {
@@ -25,7 +23,6 @@
                 this._setupEvents();
             }
         }
-
         /**
          * Start listening for error events.
          * @return {void}
@@ -51,8 +48,8 @@
          * @return {AllErrorHandler}  Returns a AllErrorHandler constructor.
          */
         noConflict() {
-            root.AllErrorHandler = previous_mymodule
-            return AllErrorHandler
+            root.AllErrorHandler = previous_mymodule;
+            return AllErrorHandler;
         }
 
         /**
@@ -68,8 +65,8 @@
         }
 
         /**
-         * It will attatch or detatch the listening event based on the given parametter;
-         * @param {Boolean} attatch - if true the object will start listening. 
+         * It will attatch or detatch the listening event based on the given parameter;
+         * @param {Boolean} attatch - if true the object will start listening.
          * @return {void}
          */
         _setupEvents(attatch = true) {
@@ -79,26 +76,27 @@
                 if (typeof window !== "undefined") {
                     window.addEventListener("error", this._callback);
                 } else {
-                    process.on('uncaughtException', this._callback);
+                    process.on("uncaughtException", this._callback);
                 }
             } else {
                 if (typeof window !== "undefined") {
                     window.removeEventListener("error", this._callback);
                 } else {
-                    process.removeListener('uncaughtException', this._callback);
+                    process.removeListener("uncaughtException", this._callback);
                 }
             }
         }
     }
-    if (typeof define === 'function' && define.amd) {
-        define([], AllErrorHandler);
-    } else if (typeof exports !== 'undefined') {
-        if (typeof module !== 'undefined' && module.exports) {
-            exports = module.exports = AllErrorHandler
+    if (typeof define === "function" && define.amd) {
+        define([], function() {
+            return (root.AllErrorHandler = AllErrorHandler);
+        });
+    } else if (typeof exports !== "undefined") {
+        if (typeof module !== "undefined" && module.exports) {
+            exports = module.exports = AllErrorHandler;
         }
-        exports.AllErrorHandler = AllErrorHandler
+        exports.AllErrorHandler = AllErrorHandler;
     } else {
-        root.AllErrorHandler = AllErrorHandler
+        root.AllErrorHandler = AllErrorHandler;
     }
-}).call(this);
-
+}.call(typeof self !== "undefined" ? self : this));
